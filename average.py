3a0acd07ba99b147123f69d435e04456c9b501cb
MAINA ERIC N <123147966+mainaeric3688@users.noreply.github.com>
2023-05-15 03:52:22 -0700

    Add files via upload
    The first assignment on Introduction to python programming

---
+++ average.py
@@ -0,0 +1,10 @@
+'''
+second code in this first class in intr to programming
+the code was meant for assignment
+'''
+first_number=int(input("Enter the first number; "))
+second_number=int(input("enter the second number; "))
+if(first_number>second_number): 
+    print("First_number is larger")
+elif(second_number>first_number):
+    print("second_number is larger")
