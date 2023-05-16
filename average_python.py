669ccc0820c81f993175e17e52d1f9d73bf60ee4
MAINA ERIC N <123147966+mainaeric3688@users.noreply.github.com>
2023-05-16 11:14:08 +0300

    Add files via upload
    The assignment on calculating the average of two numbers by propmting the user to input to numbers

---
+++ average_python.py
@@ -0,0 +1,10 @@
+'''
+ python is an enjoyable programming code
+ the assignment on calculating the average of two numbers
+ the program will prompt the user to input two numbers
+ Calculate the average of the two numbers
+'''
+first_number=int(input("enter the first number="))
+second_number=int(input("enter thesecond number="))
+avg=(first_number+second_number)/2
+print("average of rwo numbers=%1f"%avg)
