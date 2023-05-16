af4203eb18814453d9eef0f304842b4cfc8f2bc6
MAINA ERIC N <123147966+mainaeric3688@users.noreply.github.com>
2023-05-16 12:25:09 +0300

    Add files via upload
    The first python program that outputs welcoming remarks

---
+++ Welcome_python.py
@@ -0,0 +1,8 @@
+'''
+ the assignment on calculating the average of two numbers
+ python is an enjoyable programming code 
+'''
+first_number=int(input("enter the first number="))
+second_number=int(input("enter thesecond number="))
+avg=(first_number+second_number)/2      
+print("the average of the two numbers=%1f"%avg)
