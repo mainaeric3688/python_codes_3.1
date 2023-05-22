9c5979e623e97e6d6aa32ba67581f7b4fd21a813
MAINA ERIC N <123147966+mainaeric3688@users.noreply.github.com>
2023-05-22 13:43:26 +0300

    Add files via upload
    A program to check the divisibility of 5,7,11

---
+++ Divisibility_test - Copy.py
@@ -0,0 +1,29 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it
+I love progamming in python
+A program to check the divisibility of 5,7,11
+
+'''
+a=10
+b=14
+c=15
+if a%5==0:
+    print("a is divisible by 5")
+else:
+    print("a is not divisible by 5")
+    
+b=14
+if b%7==0:
+    print("b is divisible by 7")
+else:
+    print("b is not divisible by 7")
+      
+c=15
+if b%11==0:
+    print("c is divisible by 11")
+else:
+    print("c is not divisible by 11")
+      
