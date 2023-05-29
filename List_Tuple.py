8e70fbab8af6c8f4f8c0637977483f40a4adad47
MAINA ERIC N <123147966+mainaeric3688@users.noreply.github.com>
2023-05-29 03:06:42 -0700

    Add files via upload
    Python collections (List, Tuple)

---
+++ List_Tuple.py
@@ -0,0 +1,15 @@
+'''
+Python programming
+'''
+#list
+Students=["Eric","Jeremy","Juma","John"]
+a=Students
+print(a)
+print(type(a))
+#tuple
+fruits=("Cherry","Oranges","Melon","Passion")
+b=fruits
+print(b)
+print(type(b))
+
+
