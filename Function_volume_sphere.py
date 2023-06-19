2ee8d944e1b56f6bbd835c77566244e94f76cda7
MAINA ERIC N <123147966+mainaeric3688@users.noreply.github.com>
2023-06-19 13:20:05 +0300

    Add files via upload
    Function> Volume of a sphere

---
+++ Function_volume_sphere.py
@@ -0,0 +1,11 @@
+'''
+python program to calculate the volume of a sphere
+'''
+def volume(radius):
+  volume=4/3*(3.142*radius**3)
+  return volume
+  #calling the function
+volume=(3.142*7**3)*4/3 
+#output
+print("volume of the sphere is="+str(volume))    
+ 
