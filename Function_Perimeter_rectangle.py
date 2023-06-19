f2afed50589b357acdecd1eced58c980c1824296
MAINA ERIC N <123147966+mainaeric3688@users.noreply.github.com>
2023-06-19 13:34:10 +0300

    Add files via upload
    Function> perimeter of a rectangle

---
+++ Function_Perimeter_rectangle.py
@@ -0,0 +1,8 @@
+def perimeter(length,width):
+  rectangle((length+width)*2)
+  return perimeter
+#ca;;ing the function
+rectangle=(15+8)*2
+#output
+print("perimeter of the rectangle is="+str(rectangle))    
+ 
